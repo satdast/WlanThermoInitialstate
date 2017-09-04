5b1198663a84e4d78ef418d4d5fef5b92a269ee5
satdast <31583682+satdast@users.noreply.github.com>
2017-09-04 09:08:01 +0200

    Compare befor sending
    Neue Version welche die Daten nur sendet wenn auch einen Änderung statt gefunden hat.

--- WlanThermoInitialstate.py
+++ WlanThermoInitialstate.py
@@ -1,14 +1,14 @@
+import sys
 import os
 import urllib2
 import json
-import glob
-import time
+import mmap, codecs
 from ISStreamer.Streamer import Streamer
 
 # --------- User Settings ---------
-BUCKET_NAME = "xxxxxxxxx"
-BUCKET_KEY = "xxxxxxxx"
-ACCESS_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+BUCKET_NAME = "xxxxxxxxxxxxxxxx"
+BUCKET_KEY = "xxxxxxxxxxxxxxxx"
+ACCESS_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # ---------- depencies -----------------------
 # run the following command to aktivate ISStreamer for Inital State:
@@ -22,6 +22,22 @@ ACCESS_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 #                           ad your path here
 
 # ---------- getting Data -----------------------
+def read_loc_json():
+	try: #pruefen der Datei und lesen des Inhaltes
+		with open('./WTdata.json') as f:    
+			data= json.load(f)
+	except IOError: # Wenn die Datei nicht vorhanden ist
+		print('File not exists!') # Meldung ausgeben
+		return[]	# leeres dict zurueckgeben
+	f.close()
+	return data
+		
+def write_loc_json(data):
+	# write a simple example file
+	with open('./WTdata.json', 'w') as f:
+		json.dump(data, codecs.getwriter('utf-8')(f), ensure_ascii=False)	
+	f.close()
+	
 def get_values():
     api_conditions_url = "http://localhost/app.php"
     try:
@@ -34,57 +50,101 @@ def get_values():
     return json.loads(json_conditions)
 
 def main():
-    # -------------- WlanThermo --------------
-    values = get_values()
-    if ('temp_unit' not in values):
-        print "Error! Wlanthermo app.php reading failed!"
-        exit()
-    else:
-        # init ISStreamer
-        streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
+	# -------------- WlanThermo --------------
+	#neue Daten lesen
+	values = get_values()
+	
+	#alte Daten von file lesen
+	values_old = read_loc_json()
+	
+	# pruefen auf inhalt
+	force_new_data = ('temp_unit' not in values_old)
+	
+	
+	force_data = len(sys.argv) > 1
+	
+	# Manipulieren einzelner Werte
+	values['cpu_load'] = round(values['cpu_load'],2)
+	if values['temp_unit'] == 'celsius':
+		values['temp_unit'] = "C"
+	else:
+		values['temp_unit'] = "F"
 
-        # Calculate Values
-        if values['temp_unit'] == 'celsius':
-           unit = "C"
-        else:
-           unit = "F"
-   
-        # Stream valid values to Initial State
-        streamer.log("temp_unit", unit)
-        streamer.log("cpu_load", round(values['cpu_load'],2))
-        streamer.log("cpu_temp", values['cpu_temp'])
-       
-        # Stream Channel-data
-        for x in values['channel']:
-            channel = values['channel'][x]
-            if channel['state'] == 'ok' and channel['show']:
-                temp = channel['temp']
-                temp_min = channel['temp_min']
-                temp_max = channel['temp_max']
-                streamer.log("Ch_"  + x + '_Name', channel['name'])
-                streamer.log("Ch_"  + x + '_Temp', temp )
-                streamer.log("Ch_"  + x + '_Min', temp_min )
-                streamer.log("Ch_"  + x + '_Max', temp_max )
-                #add channel datapoints here
-               
-        # Stream PitMaster 1
-        if values['pit']['enabled']:
-            streamer.log("Pit1_setpoint", values['pit']['setpoint'])
-            streamer.log("Pit1_current", values['pit']['current'])
-            streamer.log("Pit1_control_out", values['pit']['control_out'])
-            streamer.log("Pit1_Last", values['pit']['timestamp'])
-           
-        # Stream PitMaster 2
-        if ('pit2' in values):
-            if values['pit2']['enabled']:
-                streamer.log("Pit2_setpoint", values['pit2']['setpoint'])
-                streamer.log("Pit2_current", values['pit2']['current'])
-                streamer.log("Pit2_control_out", values['pit2']['control_out'])
-                streamer.log("Pit2_Last", values['pit2']['timestamp'])
-       
-        #add other datapoints here
+	print('force: ' + str(force_data))	
+	#erneute Pruefung der aktualdaten zur weiteren ausfuehrung
+	if ('temp_unit' not in values):
+		print "Error! Wlanthermo app.php reading failed!"
+		exit()
+	else:
+		# init ISStreamer
+		streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
-        streamer.flush()
+		# Variablen durcharbeiten 
+		for x in values:  #alle Basis Elemente durcharbeiten
+			if (x == 'pit' or x == 'pit2'): #pitmaster signale
+				for y in values[x]:
+					new_data = False
+					if force_new_data:
+						new_data = True
+					elif force_data:
+						if str(y) == 'setpoint':
+							new_data = True
+					else:
+						new_data= not (values[x][y] == values_old[x][y])
+					
+					if 'timestamp' in y:
+						new_data = False
+					
+					if new_data:
+						#print(str(x) + '_' + str(y) + ': ' + str(values[x][y]))
+						name = str(x) + '_' + str(y)
+						value = values[x][y]
+						streamer.log(name, value)
+						
+			elif (x == 'channel'):  #alle Temperatur Kanaele durcharbeiten
+				for y in values[x]:
+					for z in values[x][y]:
+						new_data = False
+						if force_new_data:
+							new_data = True
+						elif force_data:
+							if (values[x][y]['state'] == 'ok'):
+								if str(z)[:4] == 'temp':
+									new_data = True
+						else:
+							new_data= not (values[x][y][z] == values_old[x][y][z])
+				
+						if new_data:
+							#print(str(x)[:2] + str('0'+ y)[:2] + '_' + str(z) + ': ' + str(values[x][y][z]))
+							name = str(x)[:2] + str('0'+ y)[:2] + '_' + str(z)
+							value = values[x][y][z]
+							streamer.log(name, value)
+				
+			else:  # alle anderen Signale
+				new_data = False
+				if force_new_data:
+					new_data = True
+				elif (x == 'timestamp'):
+					new_data = False
+				else:
+					new_data= not (values[x] == values_old[x])
+				
+				if new_data:
+						#print(str(x) + ': ' + str(values[x]))
+						name = str(x)
+						value = values[x]
+						streamer.log(name, value)
+						
+	print('done')
+	try:
+		streamer.flush()
+	except Exception:
+		print('Daten senden nicht moeglich!')
+		exit() # hier wird abgebrochen, damit der Zischenspeicher mit Initialstate synchron bleibt.
+		
+	# schreiben der lokalen Datei
+	write_loc_json(values)
+	
 
 if __name__ == "__main__":
     main()
