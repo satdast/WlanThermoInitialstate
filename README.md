a5d8fc973e430d4ef079820772d6fd00707ce702
satdast <31583682+satdast@users.noreply.github.com>
2017-09-04 14:12:06 +0200

    External Konfig added

--- README.md
+++ README.md
@@ -3,4 +3,19 @@ Schnittstelle vom WlanThermo zu Initiatlstate
 
 dieses Script erzeugt eine Datei zum Zwischenspeichern der letzten Daten welche an Initialstate gesendet wurden.
 
+# Commandlineparameter:
+Daten nicht senden
+/nc       keine CPU-Daten senden
+/np       keine Pitmasterdaten senden
 
+Daten zwingend senden
+/ft       Zusatzwerte der Kanaele senden (min/max)        
+/fa       Datei wird gelöscht und Daten werden neu gesendet.
+
+löschen der Zwischenspeicher Datei
+/dT       die Temporaere Datei wird gelöscht,
+          beim nächsten Aufruf findet automatisch ein INIT statt.
+          Es werden auch keine Daten gesendet.
+
+/eC=Path_to_file  
+          External Credentials BenutzerDaten von externer Datei laden
--- WlanThermoInitialstate.py
+++ WlanThermoInitialstate.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import sys
 import os
 import urllib2
@@ -9,6 +10,14 @@ from ISStreamer.Streamer import Streamer
 BUCKET_NAME = "xxxxxxxxxxxxxxxx"
 BUCKET_KEY = "xxxxxxxxxxxxxxxx"
 ACCESS_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# --------- local Path ---------
+myfile='./WTdata.json'
+
+# --------- local Tags ---------
+force_data = False
+sendCPU = True
+sendPit = True
 
 # ---------- depencies -----------------------
 # run the following command to aktivate ISStreamer for Inital State:
@@ -24,24 +33,31 @@ ACCESS_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # ---------- getting Data -----------------------
 def read_loc_json():
 	try: #pruefen der Datei und lesen des Inhaltes
-		with open('./WTdata.json') as f:    
+		with open(myfile) as f:
 			data= json.load(f)
 	except IOError: # Wenn die Datei nicht vorhanden ist
 		print('File not exists!') # Meldung ausgeben
 		return[]	# leeres dict zurueckgeben
 	f.close()
 	return data
-		
+
 def write_loc_json(data):
-	# write a simple example file
-	with open('./WTdata.json', 'w') as f:
-		json.dump(data, codecs.getwriter('utf-8')(f), ensure_ascii=False)	
+	# schreiben der Datendatei
+	with open(myfile, 'w') as f:
+		json.dump(data, codecs.getwriter('utf-8')(f), ensure_ascii=False)
 	f.close()
-	
+
+def delete_loc_json():
+	## if file exists, delete it ##
+	if os.path.isfile(myfile):
+		os.remove(myfile)
+	else:    ## Show an error ##
+		print("Error: %s file not found" % myfile)
+
 def get_values():
     api_conditions_url = "http://localhost/app.php"
     try:
-        f = urllib2.urlopen(api_conditions_url)
+		 f = urllib2.urlopen(api_conditions_url)
     except:
         print "Failed to get conditions"
         return []
@@ -49,20 +65,57 @@ def get_values():
     f.close()
     return json.loads(json_conditions)
 
+def get_ext_config(cfgpath):
+	import ConfigParser
+	global myfile
+	global sendCPU
+	global sendPit
+	cfg = ConfigParser.ConfigParser()
+	cfg.read(cfgpath)
+	myfile = cfg.get('Local','Temp_File')
+	sendCPU = cfg.get('Options','sendCPU')
+	sendPit = cfg.get('Options','sendPit')
+
 def main():
+	global sendCPU
+	global sendPit
+
+	# -------------- Kommandozeilen Parameter --------------
+	for x in range(1, len(sys.argv)):
+		print('Parameter ' + str(x) + ': ' +sys.argv[x])
+		if sys.argv[x] == '/dT' or sys.argv[x] == '/fa' :
+			delete_loc_json()
+		elif sys.argv[x] == '/ft':
+			force_data = True
+		elif sys.argv[x] == '/nc':
+			sendCPU = True
+		elif sys.argv[x] == '/np':
+			sendPit = True
+		elif '/eC' == sys.argv[x][:3]:
+			arg = sys.argv[x]
+			cfgpath= arg.split('=')
+			if os.path.isfile(cfgpath[1]):
+				get_ext_config(cfgpath[1])
+			else:
+				print('Parameter /eC File %s not exists!' % cfgpath[1])
+			print('Path: ' + myfile)
+			print('CPU-Daten: ' + str(sendCPU))
+			print('Pit-Daten: ' + str(sendPit))
+			exit()
+		else:
+			print('Wrong Parameter %s in Commandline' % sys.argv[x])
+			exit()
+
 	# -------------- WlanThermo --------------
 	#neue Daten lesen
 	values = get_values()
-	
+
 	#alte Daten von file lesen
 	values_old = read_loc_json()
-	
+
 	# pruefen auf inhalt
 	force_new_data = ('temp_unit' not in values_old)
-	
-	
-	force_data = len(sys.argv) > 1
-	
+
 	# Manipulieren einzelner Werte
 	values['cpu_load'] = round(values['cpu_load'],2)
 	if values['temp_unit'] == 'celsius':
@@ -70,7 +123,7 @@ def main():
 	else:
 		values['temp_unit'] = "F"
 
-	print('force: ' + str(force_data))	
+	print('force: ' + str(force_data))
 	#erneute Pruefung der aktualdaten zur weiteren ausfuehrung
 	if ('temp_unit' not in values):
 		print "Error! Wlanthermo app.php reading failed!"
@@ -79,28 +132,29 @@ def main():
 		# init ISStreamer
 		streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
-		# Variablen durcharbeiten 
+		# Variablen durcharbeiten
 		for x in values:  #alle Basis Elemente durcharbeiten
-			if (x == 'pit' or x == 'pit2'): #pitmaster signale
-				for y in values[x]:
-					new_data = False
-					if force_new_data:
-						new_data = True
-					elif force_data:
-						if str(y) == 'setpoint':
-							new_data = True
-					else:
-						new_data= not (values[x][y] == values_old[x][y])
-					
-					if 'timestamp' in y:
+			if ('pit' == str(x)[:3]): #pitmaster signale
+				if sendPit:
+					for y in values[x]:
 						new_data = False
-					
-					if new_data:
-						#print(str(x) + '_' + str(y) + ': ' + str(values[x][y]))
-						name = str(x) + '_' + str(y)
-						value = values[x][y]
-						streamer.log(name, value)
-						
+						if force_new_data:
+							new_data = True
+						elif force_data:
+							if str(y) == 'setpoint':
+								new_data = True
+						else:
+							new_data= not (values[x][y] == values_old[x][y])
+
+						if 'timestamp' in y:
+							new_data = False
+
+						if new_data:
+							#print(str(x) + '_' + str(y) + ': ' + str(values[x][y]))
+							name = str(x) + '_' + str(y)
+							value = values[x][y]
+							streamer.log(name, value)
+
 			elif (x == 'channel'):  #alle Temperatur Kanaele durcharbeiten
 				for y in values[x]:
 					for z in values[x][y]:
@@ -113,13 +167,25 @@ def main():
 									new_data = True
 						else:
 							new_data= not (values[x][y][z] == values_old[x][y][z])
-				
+
 						if new_data:
 							#print(str(x)[:2] + str('0'+ y)[:2] + '_' + str(z) + ': ' + str(values[x][y][z]))
 							name = str(x)[:2] + str('0'+ y)[:2] + '_' + str(z)
 							value = values[x][y][z]
 							streamer.log(name, value)
-				
+			elif ('cpu' == str(x)[:3]):
+				if sendCPU:
+					new_data = False
+					if force_new_data:
+						new_data = True
+					else:
+						new_data= not (values[x] == values_old[x])
+					if new_data:
+						#print(str(x) + ': ' + str(values[x]))
+						name = str(x)
+						value = values[x]
+						streamer.log(name, value)
+
 			else:  # alle anderen Signale
 				new_data = False
 				if force_new_data:
@@ -128,23 +194,23 @@ def main():
 					new_data = False
 				else:
 					new_data= not (values[x] == values_old[x])
-				
+
 				if new_data:
 						#print(str(x) + ': ' + str(values[x]))
 						name = str(x)
 						value = values[x]
 						streamer.log(name, value)
-						
+
 	print('done')
 	try:
 		streamer.flush()
 	except Exception:
 		print('Daten senden nicht moeglich!')
 		exit() # hier wird abgebrochen, damit der Zischenspeicher mit Initialstate synchron bleibt.
-		
+
 	# schreiben der lokalen Datei
 	write_loc_json(values)
-	
+
 
 if __name__ == "__main__":
     main()
